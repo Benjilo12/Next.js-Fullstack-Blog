40bbc4c09e09561d3da8f14c859c847baedced8b
Benjilo12 <benjamindarteyy@gmail.com>
2025-10-31 14:55:36 +0000

    fix: Update HeroSection to use min-h-full for better layout consistency

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -5,7 +5,7 @@ import { FlipWords } from "@/components/ui/flipwords";
 export default function HeroSection() {
   return (
     <>
-      <div className="relative mx-auto flex min-h-[screen] max-w-7xl  mt-12 md:mt-13 flex-col overflow-hidden px-4 pt-16 sm:px-6 sm:pt-20 md:pt-24 lg:px-8 lg:pt-32 xl:pt-40  ">
+      <div className="relative mx-auto flex min-h-full max-w-7xl  mt-12 md:mt-17 flex-col overflow-hidden px-4 pt-16 sm:px-6 sm:pt-20 md:pt-24 lg:px-8 lg:pt-25 xl:pt-30  ">
         <div>
           {/* Main Heading - Responsive sizing and spacing */}
           <div className="relative z-10 mx-auto mt-0 max-w-6xl text-center text-2xl font-semibold text-black sm:mt-6 sm:text-4xl md:text-4xl lg:text-6xl xl:text-7xl 2xl:text-8xl dark:text-white">
